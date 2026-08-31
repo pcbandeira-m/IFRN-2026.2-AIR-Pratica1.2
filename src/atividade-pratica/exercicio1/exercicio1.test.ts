824a7622fb5169bb97959f8dad05bbf3803f65b8
Patricia <pcbandeira.m@gmail.com>
2026-08-31 01:36:21 +0000

    fix: correção do import no teste do exercicio1

--- src/atividade-pratica/exercicio1/exercicio1.test.ts
+++ src/atividade-pratica/exercicio1/exercicio1.test.ts
@@ -1,4 +1,4 @@
-import { elementosAoQuadradoFpr, elementosAoQuadradoEach } from "./exercicio1";
+import { elementosAoQuadradoFor, elementosAoQuadradoEach } from "./exercicio1";
 
 const numeros: number[] = [3, 5, 7, 3, 8, 9, 1];
 const resultadoEsperado: number[] = [9, 25, 49, 9, 64, 81, 1];
